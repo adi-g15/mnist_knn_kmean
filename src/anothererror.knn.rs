ce83c94642a0975c80ea83e3e5de744dd4e7e464
adi-g15 <ag15035@gmail.com>
2021-10-01 21:46:50 +0530

    Errors Errors Everywhere -> That's what rust tells me eveytime

---
+++ src/anothererror.knn.rs
@@ -0,0 +1,46 @@
+use crate::observation::Observation;
+use rand::{thread_rng, seq::SliceRandom};
+
+pub struct KNN<'a> {
+    pub k: u32,
+    dataset: &'a [Observation],  // complete dataset
+    pub training_set: &'a [Observation],
+    pub validation_set: &'a [Observation],
+    pub test_set: &'a [Observation]
+}
+
+impl KNN<'_> {
+    fn test_train_split<'a>(dataset: &'a mut [Observation]) ->
+        (&'a [Observation], &'a [Observation], &'a [Observation]) {
+        const TRAIN_SET_RATIO: f32 = 0.75;   // use 75% of dataset for training
+        const VALIDATION_SET_RATIO: f32 = 0.10;
+        const TEST_SET_RATIO: f32 = 0.15;
+
+        let num_elements_train = TRAIN_SET_RATIO * dataset.len() as f32;
+        let num_elements_test  = TEST_SET_RATIO * dataset.len() as f32;
+
+        // TODO: Later completely shuffle the vector instead of partial_shuffle
+
+        let mut rng = thread_rng();
+        let (training_set, rest_of_data) = {
+            dataset.partial_shuffle(&mut rng, num_elements_train as usize)
+        };
+        let (validation_set, test_set) = rest_of_data.partial_shuffle(&mut rng, num_elements_test as usize);
+
+        (training_set, validation_set, test_set)
+    }
+
+    pub fn new<'a>(dataset: &'a mut [Observation]) -> KNN<'a> {
+        let (training_set, validation_set, test_set)
+            = KNN::test_train_split(dataset);
+
+        KNN {
+            k: 5,   // Won't be used, k needs to be calculated later
+            dataset,
+            training_set,
+            validation_set,
+            test_set
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod observation;
+mod knn;
 
 use observation::Observation;
+use knn::KNN;
 use std::fs::File;
+use std::io::BufReader;
 use std::io::Read;  // trait required for read functions on file
 
 fn check_if_magic_matches(bytes: &[u8;4], magic_number: i32) -> Result<(),String> {
@@ -14,14 +17,32 @@ fn check_if_magic_matches(bytes: &[u8;4], magic_number: i32) -> Result<(),String
 }
 
 fn main() {
+    // Note that these datasets will just be merged and chosing training and
+    // testing set is upto the algorithm, not using the MNIST files ratio (6/1)
     let mut train_dataset: Vec<Observation> = Vec::new();
     let mut test_dataset: Vec<Observation> = Vec::new();
 
+    /* Validation set is what we use to validate that our model is trained to
+     * our liking, for eg. say when training a neural network we will feed
+     * forward and back prop as many times as we need until the performace on
+     * the validation set reaches a threshold, then we'll put the test data into
+     * the network and see what the performance is*/
+    // let mut validation_dataset = Vec::new();
+
     // Open all 4 dataset files
-    let mut training_set_label_file = File::open("dataset/train-labels-idx1-ubyte").unwrap();
-    let mut training_set_image_file = File::open("dataset/train-images-idx3-ubyte").unwrap();
-    let mut test_set_label_file = File::open("dataset/t10k-labels-idx1-ubyte").unwrap();
-    let mut test_set_image_file = File::open("dataset/t10k-images-idx3-ubyte").unwrap();
+    // Using Buffered Reader, from doc of io::BufReader:
+    // BufReader<R> can improve the speed of programs that make small and 
+    // repeated read calls to the same file or network socket. It doesn't help
+    // when reading very large amounts at once, or reading a few times.
+    // Statistics: In case of this dataset, it saved 0.035 seconds on average
+    let mut training_set_label_file = 
+        BufReader::new(File::open("dataset/train-labels-idx1-ubyte").unwrap());
+    let mut training_set_image_file = 
+        BufReader::new(File::open("dataset/train-images-idx3-ubyte").unwrap());
+    let mut test_set_label_file = 
+        BufReader::new(File::open("dataset/t10k-labels-idx1-ubyte").unwrap());
+    let mut test_set_image_file = 
+        BufReader::new(File::open("dataset/t10k-images-idx3-ubyte").unwrap());
 
     // All these check_if_magic_matches, are to make sure we are reading with correct endianness,
     // since the bytes in MNIST dataset are in MSB first (high endian), so on processors such as
@@ -81,9 +102,13 @@ fn main() {
     let mut image_bytes = Vec::new();
     image_bytes.resize( (num_rows_train_image * num_cols_train_image) as usize, 0u8);
 
+    let mut labels = std::collections::HashSet::new();
+
     for _i in 0..num_images_train {
         training_set_label_file.read_exact(&mut one_byte).unwrap();
         let label = u8::from_be_bytes(one_byte);
+
+        labels.insert(label);
 
         training_set_image_file.read_exact(&mut image_bytes).unwrap();
         
@@ -96,6 +121,8 @@ fn main() {
         test_set_label_file.read_exact(&mut one_byte).unwrap();
         let label = u8::from_be_bytes(one_byte);
 
+        labels.insert(label);
+
         test_set_image_file.read_exact(&mut image_bytes).unwrap();
 
         test_dataset.push(
@@ -105,4 +132,11 @@ fn main() {
 
     println!("Successfully read: {} train observations, and {} test observations !", train_dataset.len(), test_dataset.len() );
 
+    println!("Total labels read: {}", labels.len());
+    println!("Labels: {:?}", labels);
+
+    let mut dataset = train_dataset;
+    dataset.append(&mut test_dataset);
+
+    let knn_model = KNN::new(&mut dataset);
 }
